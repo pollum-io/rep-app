dd6fbc2bd322ead171e1cf65aa0c86aaa46245fc
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2023-05-08 15:51:25 -0300

    fix: test8

--- helpers/translation.ts
+++ helpers/translation.ts
@@ -1,67 +1,39 @@
-import i18next from "i18next";
-import { initReactI18next } from "react-i18next";
-import HttpApi from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
-
-export const availableLanguages = ["en", "pt-br"];
-export const defaultLocale = "pt-br";
-const LOCALE_VERSION = "1.5.1";
-
-const determineLngFn = (code: string): string => {
-	let { language } = i18next;
-
-	if (!code || code.length === 0) {
-		language = defaultLocale;
-
-		return language;
-	}
-
-	// Full locale match
-	if (availableLanguages.includes(code.toLowerCase())) {
-		language = code.toLowerCase();
-
-		return language;
-	}
-
-	// Base locale match
-	const codeBase = code.split("-")[0].toLowerCase();
-	if (availableLanguages.includes(codeBase)) {
-		language = codeBase;
-
-		return language;
-	}
-
-	// Fallback
-	return language;
-};
-
-i18next
-	.use(HttpApi)
-	.use(LanguageDetector)
-	.use(initReactI18next)
-	.init({
-		backend: {
-			loadPath: `/locales/{{lng}}.json`,
-			queryStringParams: { v: LOCALE_VERSION },
-		},
-		react: {
-			useSuspense: true,
-		},
-		load: "languageOnly",
-		lowerCaseLng: true,
-		fallbackLng: determineLngFn,
-		preload: [defaultLocale],
-		keySeparator: ".",
-		interpolation: { escapeValue: false },
-	});
-
-export default i18next;
-
-// import React from "react";
-// import { useTranslation, initReactI18next } from "react-i18next";
 // import i18next from "i18next";
+// import { initReactI18next } from "react-i18next";
 // import HttpApi from "i18next-http-backend";
 // import LanguageDetector from "i18next-browser-languagedetector";
+
+// export const availableLanguages = ["en", "pt-br"];
+// export const defaultLocale = "pt-br";
+// const LOCALE_VERSION = "1.5.1";
+
+// const determineLngFn = (code: string): string => {
+// 	let { language } = i18next;
+
+// 	if (!code || code.length === 0) {
+// 		language = defaultLocale;
+
+// 		return language;
+// 	}
+
+// 	// Full locale match
+// 	if (availableLanguages.includes(code.toLowerCase())) {
+// 		language = code.toLowerCase();
+
+// 		return language;
+// 	}
+
+// 	// Base locale match
+// 	const codeBase = code.split("-")[0].toLowerCase();
+// 	if (availableLanguages.includes(codeBase)) {
+// 		language = codeBase;
+
+// 		return language;
+// 	}
+
+// 	// Fallback
+// 	return language;
+// };
 
 // i18next
 // 	.use(HttpApi)
@@ -69,15 +41,44 @@ export default i18next;
 // 	.use(initReactI18next)
 // 	.init({
 // 		backend: {
-// 			loadPath: "/locales/{{lng}}/{{ns}}.json",
+// 			loadPath: `/locales/{{lng}}.json`,
+// 			queryStringParams: { v: LOCALE_VERSION },
 // 		},
-// 		fallbackLng: "en",
-// 		debug: true,
-// 		detection: {
-// 			order: ["cookie", "navigator"],
-// 			caches: ["cookie"],
+// 		react: {
+// 			useSuspense: true,
 // 		},
-// 		interpolation: {
-// 			escapeValue: false,
-// 		},
+// 		load: "languageOnly",
+// 		lowerCaseLng: true,
+// 		fallbackLng: determineLngFn,
+// 		preload: [defaultLocale],
+// 		keySeparator: ".",
+// 		interpolation: { escapeValue: false },
 // 	});
+
+// export default i18next;
+
+import React from "react";
+import { useTranslation, initReactI18next } from "react-i18next";
+import i18next from "i18next";
+import HttpApi from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18next
+	.use(HttpApi)
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		backend: {
+			loadPath: "/locales/{{lng}}/{{ns}}.json",
+		},
+		fallbackLng: "en",
+		debug: true,
+		detection: {
+			order: ["cookie", "navigator"],
+			caches: ["cookie"],
+		},
+		interpolation: {
+			escapeValue: false,
+		},
+		keySeparator: ".",
+	});
