ea8e6e24b7b687acc782e7a20f7ec1dde2589e4f
Gabriel Prediger <61945626+GabrielPrediger@users.noreply.github.com>
2023-05-08 15:54:28 -0300

    fix: test9

--- helpers/translation.ts
+++ helpers/translation.ts
@@ -63,6 +63,38 @@ import i18next from "i18next";
 import HttpApi from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
+export const availableLanguages = ["en", "pt-br"];
+export const defaultLocale = "pt-br";
+const LOCALE_VERSION = "1.5.1";
+
+const determineLngFn = (code: string): string => {
+	let { language } = i18next;
+
+	if (!code || code.length === 0) {
+		language = defaultLocale;
+
+		return language;
+	}
+
+	// Full locale match
+	if (availableLanguages.includes(code.toLowerCase())) {
+		language = code.toLowerCase();
+
+		return language;
+	}
+
+	// Base locale match
+	const codeBase = code.split("-")[0].toLowerCase();
+	if (availableLanguages.includes(codeBase)) {
+		language = codeBase;
+
+		return language;
+	}
+
+	// Fallback
+	return language;
+};
+
 i18next
 	.use(HttpApi)
 	.use(LanguageDetector)
@@ -71,7 +103,7 @@ i18next
 		backend: {
 			loadPath: "/locales/{{lng}}/{{ns}}.json",
 		},
-		fallbackLng: "en",
+		fallbackLng: determineLngFn,
 		debug: true,
 		detection: {
 			order: ["cookie", "navigator"],
